a9414a8eda57441a9e0996cc2a3916243c2dff83
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-04-22 16:38:33 +0600

    Declare Nested If-else

--- index.php
+++ index.php
@@ -40,4 +40,38 @@ else{
 
 
 
+// Nested if else
+
+$examScore=80;
+$grade="";
+
+if($examScore >=90){
+    $grade="A";
+}
+else{
+    if($grade>=80){
+        $grade="B";
+    }
+    else{
+        if($examScore >=70){
+            $grade="C";
+        }
+        else{
+            if($examScore >=60){
+                $grade="D";
+            }
+            else{
+                if($examScore >=50){
+                    $grade="E";
+                }
+                else{
+                    $grade="F";
+                }
+            }
+        }
+    }
+}
+
+
+
 ?>
