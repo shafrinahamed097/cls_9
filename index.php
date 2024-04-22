0d16dd1122fb9554ff7ab1ed5f6c1bced6a45481
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-04-22 12:37:40 +0600

    simple if-else, if-else ladder 55.56

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+// simple if-else
+$userName="abc";
+$pass="123";
+
+if($userName=="abc" && $pass=="123"){
+    echo "Login Success";
+}
+else{
+    echo "Login Failed";
+}
+
+// if else ladder
+
+
+$score=75;
+
+if($score >=90){
+    echo "A";
+}
+elseif($score >=80){
+    echo "B";
+}
+elseif($score >=70){
+    echo "C";
+}
+elseif($score >=60){
+    echo "D";
+}
+
+elseif($score >=50){
+    echo "E";
+}
+
+else{
+    echo "F";
+}
+
+
+
+
+?>
